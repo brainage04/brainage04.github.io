6178ae472f21794069a4a748f905b2047cd31f54
brainage04 <thomasmcmah21@gmail.com>
2026-09-04 16:19:20 +1000

    Optimize profile and social assets

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -8,24 +8,31 @@ export type SocialLink = {
 
 export const socials: SocialLink[] = [
   {
+    name: 'Email Thomas',
+    href: 'mailto:thomasmcmah21@gmail.com',
+    icon: '/icons/mail.svg',
+    iconWidth: 24,
+    iconHeight: 24,
+  },
+  {
     name: 'GitHub',
     href: 'https://github.com/brainage04',
-    icon: '/assets/images/socials/github.png',
-    iconWidth: 230,
-    iconHeight: 225,
+    icon: '/icons/github.svg',
+    iconWidth: 24,
+    iconHeight: 24,
   },
   {
     name: 'Twitter / X',
     href: 'https://x.com/brainage19',
-    icon: '/assets/images/socials/x.png',
-    iconWidth: 2400,
-    iconHeight: 2453,
+    icon: '/icons/x.svg',
+    iconWidth: 1200,
+    iconHeight: 1227,
   },
   {
     name: 'Discord',
     href: 'https://discord.com/users/brainage04',
-    icon: '/assets/images/socials/discord.png',
-    iconWidth: 619,
-    iconHeight: 469,
+    icon: '/icons/discord.svg',
+    iconWidth: 64,
+    iconHeight: 48,
   },
 ];
